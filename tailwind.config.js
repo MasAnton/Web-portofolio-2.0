b879987d9aa440505102343e85c370b225d5b45e
MasAnton <antonprayogo002@gmail.com>
2022-12-13 11:33:52 +0700

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,18 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
         gelap: "#01141A",
         biru: "#2FC3FF",
         putih: "#FFFFFF",
+      },
+      screens: {
+        "2xl": "1320px",
       },
     },
   },
